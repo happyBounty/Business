1ecf1558195fc371964b83e63544bf79a5d141b1
OliveBounty <oliveodobounty@gmail.com>
2021-05-09 23:37:59 +0900

    logincomponent key event binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,5 +11,27 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  showData($event) {
 
+    console.log("LogIn component:loginbuttonclicked\n" + "username:" + this.username + "\n" + "password:" + this.password + "\n" + "email:" + this.email);
+  }
+  username = '';
+  password = '';
+  email = '';
+
+  onKey(event: any) {
+    switch (event.target.id) {
+      case "username":
+        this.username = event.target.value;
+        break;
+      case "password":
+        this.password = event.target.value;
+        break;
+      case "email":
+        this.email = event.target.value;
+        break;
+    }
+    console.log(event.target.id + ":" + event.target.value);
+
+  }
 }
